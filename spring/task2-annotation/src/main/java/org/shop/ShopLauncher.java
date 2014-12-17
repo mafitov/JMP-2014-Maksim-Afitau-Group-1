faf8556e8fcf30bbec663fbae3dd99a7e047b452
viacheslav-bogdanov <viacheslav_bogdanov@epam.com>
2014-12-17 22:02:53 +0300

    Spring task 2 - annotation config

---
+++ spring/task2-annotation/src/main/java/org/shop/ShopLauncher.java
@@ -0,0 +1,52 @@
+package org.shop;
+
+
+import org.shop.api.*;
+import org.shop.data.Item;
+import org.shop.data.Order;
+import org.shop.data.Product;
+import org.shop.data.Proposal;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * The ShopLauncher class.
+ */
+public class ShopLauncher {
+    
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        ProductService productService = (ProductService) context.getBean("productService");
+
+        OrderService orderService = (OrderService) context.getBean("orderService");
+
+        ItemService itemService = (ItemService) context.getBean("itemService");
+
+//        UserService userService = (UserService) context.getBean("clientService");
+        UserService userService = (UserService) context.getBean("userService");
+
+        ProposalService proposalService = context.getBean(ProposalService.class);
+
+//        DataInitializer dataInitializer = context.getBean(DataInitializer.class);
+//        dataInitializer.initData();
+
+        Product galaxy = productService.getProductsByName("Samsung Galaxy Tab").get(0);
+        Proposal proposal = proposalService.getProposalsByProduct(galaxy).get(0);
+
+        orderService.createOrder(userService.getUserById((long) 1), proposal);
+
+        for (Order order : orderService.getOrdersByUserId((long) 1)) {
+            System.out.println(order);
+
+            for (Item item : itemService.getItemsByOrderId(order.getId())) {
+                System.out.println(item);
+            }
+        }
+    }
+}
